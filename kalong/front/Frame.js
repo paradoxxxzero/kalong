10020a2b2f5a891ce8e043d357890dac7db08f36
Florian Mounier <paradoxxx.zero@gmail.com>
2019-05-13 16:50:59 +0200

    Frames with hooks

--- kalong/front/Frame.js
+++ kalong/front/Frame.js
@@ -1,70 +1,55 @@
 import { ListItemSecondaryAction, Tooltip, Typography } from '@material-ui/core'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
-import React from 'react'
+import React, { useCallback } from 'react'
 import StarIcon from '@material-ui/icons/Star'
 
 import { setActiveFrame } from './actions'
 import Snippet from './Code/Snippet'
 
-@connect(
-  state => ({
-    activeFrame: state.activeFrame,
-  }),
-  dispatch => ({
-    selectFrame: key => dispatch(setActiveFrame(key)),
-  })
-)
-export default class Frame extends React.PureComponent {
-  constructor(props) {
-    super(props)
+export default function Frame({ frame, last }) {
+  const activeFrame = useSelector(state => state.activeFrame)
+  const dispatch = useDispatch()
+  const handleClick = useCallback(() => dispatch(setActiveFrame(frame.key)), [
+    dispatch,
+    frame,
+  ])
 
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    const { frame, selectFrame } = this.props
-    selectFrame(frame.key)
-  }
-
-  render() {
-    const { frame, activeFrame, last } = this.props
-    return (
-      <ListItem
-        button
-        divider={!last}
-        selected={frame.key === activeFrame}
-        onClick={this.handleClick}
-      >
-        <ListItemText
-          primary={frame.function}
-          secondary={
-            <>
-              <Snippet value={frame.lineSource} />
-              <Tooltip title={frame.absoluteFilename}>
-                <Typography
-                  variant="body2"
-                  display="block"
-                  component="span"
-                  noWrap
-                >
-                  {frame.filename}:{frame.lineNumber}
-                </Typography>
-              </Tooltip>
-            </>
-          }
-        />
-        <ListItemSecondaryAction>
-          {frame.active && (
-            <Tooltip title="Current Frame">
-              <div>
-                <StarIcon fontSize="small" />
-              </div>
+  return (
+    <ListItem
+      button
+      divider={!last}
+      selected={frame.key === activeFrame}
+      onClick={handleClick}
+    >
+      <ListItemText
+        primary={frame.function}
+        secondary={
+          <>
+            <Snippet value={frame.lineSource} />
+            <Tooltip title={frame.absoluteFilename}>
+              <Typography
+                variant="body2"
+                display="block"
+                component="span"
+                noWrap
+              >
+                {frame.filename}:{frame.lineNumber}
+              </Typography>
             </Tooltip>
-          )}
-        </ListItemSecondaryAction>
-      </ListItem>
-    )
-  }
+          </>
+        }
+      />
+      <ListItemSecondaryAction>
+        {frame.active && (
+          <Tooltip title="Current Frame">
+            <div>
+              <StarIcon fontSize="small" />
+            </div>
+          </Tooltip>
+        )}
+      </ListItemSecondaryAction>
+    </ListItem>
+  )
 }
--- kalong/front/Frames.js
+++ kalong/front/Frames.js
@@ -1,45 +1,30 @@
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import List from '@material-ui/core/List'
-import React from 'react'
-import equal from 'fast-deep-equal'
+import React, { useEffect } from 'react'
 
 import { getFrames, setActiveFrame } from './actions'
 import Frame from './Frame'
 
-@connect(
-  state => ({
-    activeFrame: state.activeFrame,
-    frames: state.frames,
-  }),
-  dispatch => ({
-    requestFrames: () => dispatch(getFrames()),
-    selectFrame: key => dispatch(setActiveFrame(key)),
-  })
-)
-export default class Frames extends React.PureComponent {
-  componentDidMount() {
-    const { frames, requestFrames } = this.props
-
-    if (!frames.length) {
-      requestFrames()
-    }
-  }
-
-  componentDidUpdate({ frames: oldFrames }) {
-    const { frames, selectFrame } = this.props
-    if (!equal(frames, oldFrames)) {
-      selectFrame(frames[0].key)
-    }
-  }
-
-  render() {
-    const { frames } = this.props
-    return (
-      <List>
-        {frames.map((frame, i) => (
-          <Frame key={frame.key} frame={frame} last={i === frames.length - 1} />
-        ))}
-      </List>
-    )
-  }
+export default function Frames() {
+  const frames = useSelector(state => state.frames)
+  const dispatch = useDispatch()
+  useEffect(
+    () => {
+      dispatch(getFrames())
+    },
+    [dispatch]
+  )
+  useEffect(
+    () => {
+      frames.length && dispatch(setActiveFrame(frames[0].key))
+    },
+    [dispatch, frames]
+  )
+  return (
+    <List>
+      {frames.map((frame, i) => (
+        <Frame key={frame.key} frame={frame} last={i === frames.length - 1} />
+      ))}
+    </List>
+  )
 }
