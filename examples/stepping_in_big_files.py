a605b52754b8fa66c57284478fd5fb65062b435b
Florian Mounier <florian.mounier@kozea.fr>
2020-09-22 12:21:25 +0200

    Reduce turtle length

--- examples/stepping_in_big_files.py
+++ examples/stepping_in_big_files.py
@@ -1,22 +1,10 @@
 import turtle
 
 breakpoint()
-ninja = turtle.Turtle()
+spiral = turtle.Turtle()
 
-ninja.speed(10)
-
-for i in range(180):
-    ninja.forward(100)
-    ninja.right(30)
-    ninja.forward(20)
-    ninja.left(60)
-    ninja.forward(50)
-    ninja.right(30)
-
-    ninja.penup()
-    ninja.setposition(0, 0)
-    ninja.pendown()
-
-    ninja.right(2)
+for i in range(20):
+    spiral.forward(i * 10)
+    spiral.right(144)
 
 turtle.done()
