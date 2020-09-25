3756a10fbfa58545c06d7ecd8f624d4db4d52067
Florian Mounier <florian.mounier@kozea.fr>
2020-09-25 17:40:51 +0200

    Make HMR work

--- kalong/front/index.js
+++ kalong/front/index.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux'
 import { applyMiddleware, compose, createStore } from 'redux'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
 import ThemedApp from './ThemedApp'
 import reducer from './reducer'
@@ -19,9 +19,19 @@ store.subscribe(() => {
   localStorage.setItem('history', JSON.stringify(store.getState().history))
 })
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ThemedApp />
-  </Provider>,
-  document.getElementById('root')
-)
+const renderRoot = () => {
+  render(
+    <Provider store={store}>
+      <ThemedApp />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+renderRoot()
+if (module.hot) {
+  module.hot.accept('./ThemedApp', () => renderRoot())
+  module.hot.accept('./reducer', () => {
+    store.replaceReducer(reducer)
+  })
+}
