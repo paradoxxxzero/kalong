7d38ac956101837b199255591e0f3001aa89cd7a
Florian Mounier <paradoxxx.zero@gmail.com>
2022-09-07 17:03:29 +0200

    v0.2.5

--- kalong/__init__.py
+++ kalong/__init__.py
@@ -1,5 +1,5 @@
 """A new take on debugging"""
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 import os
 import sys
 
--- src/SideDrawer.jsx
+++ src/SideDrawer.jsx
@@ -108,7 +108,7 @@ export default function SideDrawer({ rtl, open, mobile, onDrawerClose }) {
             underline="hover"
           >
             <Typography variant="subtitle1" sx={{ fontSize: '1.25em' }}>
-              v0.2.4
+              v0.2.5
             </Typography>
           </Link>
         </Box>
