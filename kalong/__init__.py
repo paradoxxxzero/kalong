ea4fccc47265178dc30b37e26976f123e5dba913
Florian Mounier <paradoxxx.zero@gmail.com>
2024-09-30 11:29:39 +0200

    0.5.1

--- kalong/__init__.py
+++ kalong/__init__.py
@@ -1,6 +1,6 @@
 """A new take on debugging"""
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 import os
 import sys
 from pathlib import Path
--- src/index.jsx
+++ src/index.jsx
@@ -21,4 +21,4 @@ if (
   )
 }
 
-export const VERSION = '0.5.0'
+export const VERSION = '0.5.1'
