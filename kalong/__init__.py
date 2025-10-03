62bd18c1aecb1bf64fb2f039a99ba41331f86158
Florian Mounier <paradoxxx.zero@gmail.com>
2025-10-03 12:44:23 +0200

    0.5.4

--- kalong/__init__.py
+++ kalong/__init__.py
@@ -1,6 +1,6 @@
 """A new take on debugging"""
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 import os
 import sys
 from pathlib import Path
@@ -111,25 +111,22 @@ def main():
 
     elif config.inject:
         kalong_dir = Path(__file__).resolve().parent.parent
-        gdb_command = (
-            ["gdb", "-p", str(config.inject), "-batch"]
-            + [
-                "-eval-command=call %s" % hook
-                for hook in [
-                    "(int) PyGILState_Ensure()",  # Getting the GIL
-                    '(int) PyRun_SimpleString("'
-                    f"print('* Kalong injection from {os.getpid()} *');"
-                    "import sys;"  # Putting kalong project directory in sys path:
-                    f"sys.path.insert(0, '{kalong_dir}');"
-                    "import kalong;"  # Setting breakpoint:
-                    "kalong.break_above(2);"
-                    '")',
-                    # Releasing the GIL with the PyGILState_Ensure handle:
-                    "(void) PyGILState_Release($1)",
-                ]
+        gdb_command = ["gdb", "-p", str(config.inject), "-batch"] + [
+            "-eval-command=call %s" % hook
+            for hook in [
+                "(int) PyGILState_Ensure()",  # Getting the GIL
+                '(int) PyRun_SimpleString("'
+                f"print('* Kalong injection from {os.getpid()} *');"
+                "import sys;"  # Putting kalong project directory in sys path:
+                f"sys.path.insert(0, '{kalong_dir}');"
+                "import kalong;"  # Setting breakpoint:
+                "kalong.break_above(2);"
+                '")',
+                # Releasing the GIL with the PyGILState_Ensure handle:
+                "(void) PyGILState_Release($1)",
             ]
-        )
-        print(f'Running: {" ".join(gdb_command)}')
+        ]
+        print(f"Running: {' '.join(gdb_command)}")
         run(gdb_command)
 
     else:
--- src/index.jsx
+++ src/index.jsx
@@ -21,4 +21,4 @@ if (
   )
 }
 
-export const VERSION = '0.5.3'
+export const VERSION = '0.5.4'
